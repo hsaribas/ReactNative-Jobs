3a25ea515c4bac339ffa875ca8c08de398df04d9
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2023-06-21 22:17:52 +0300

    commit-4

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,26 @@
-import { Stack } from 'expo-router';
-import { useCallback } from 'react';
-import { useFonts } from 'expo-font';
-import * as Splashcreen from 'expo-splash-screen';
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as Splashcreen from "expo-splash-screen";
 
 Splashcreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />;
-}
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await Splashcreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
+};
 
 export default Layout;
